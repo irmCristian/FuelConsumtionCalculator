69e8aaea9e4d22cf016d22f62aba85d846df4b70
Cristian Irimia <irm.cristi@gmail.com>
2022-06-22 14:04:19 +0300

    updated

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ const litre = document.querySelector('#litre')
 const km = document.querySelector('#km')
 const checked = document.querySelector('#checked')
 const priceFuel = document.querySelector('#priceFuel')
-const totalCost = document.querySelector('#totalCost')
+const totalGas = document.querySelector('#totalCost')
 const priceTrip = document.querySelector('#priceTrip')
 const btnn = document.querySelector('#btnn')
 
 
 btnn.addEventListener('click', function () {
-    // let results = (100 * litre.value) / km.value
     let results = (km.value * litre.value) / 100
-    let price = (priceFuel.value * litre.value) * 10
+    let price = (priceFuel.value * litre.value)
 
-    totalCost.innerHTML = results.toFixed(1)
-    priceTrip.innerHTML = price.toFixed(1)
+    totalGas.innerHTML = results.toFixed(2)
+    priceTrip.innerHTML = price.toFixed(2)
+
+    if (km.value < 100) {
+        price = price / 2
+        priceTrip.innerHTML = price.toFixed(2)
+    } else {
+        price = results * priceFuel.value
+        priceTrip.innerHTML = price.toFixed(2)
+    }
 
     if (checked.checked) {
         results = results * 2
         price = price * 2
-        totalCost.innerHTML = results.toFixed(1)
+        totalGas.innerHTML = results.toFixed(2)
         priceTrip.innerHTML = price.toFixed(2)
     }
 
